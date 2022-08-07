ab0974cd3c4cac73358b513dd21098fd1c1ce925
Danil <danil.polovinkin.1994@gmail.com>
2022-08-07 13:48:11 +0300

    added pop-up

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,11 @@
 import slider from "./slider.js";
 import showVid from "./etc.js";
+import popUp from "./popUp.js";
 
 showVid();
 
 if (document.querySelector(".foreground")) {
   slider(".foreground", " .sliderToggler", ".sliderToggler__button");
 }
+
+popUp();
